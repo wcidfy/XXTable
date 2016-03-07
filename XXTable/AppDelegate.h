6970870fec558359e246c4614d3084a7daca7b6f
lxx <listen987654321@sina.com>
2016-03-07 11:26:56 +0800

    Initial Commit

---
+++ XXTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XXTable
+//
+//  Created by lxx on 16/3/7.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XXTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXTable
+//
+//  Created by lxx on 16/3/7.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
